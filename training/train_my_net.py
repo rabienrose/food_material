882ad4730859240709c8e21426ba1c97f5bf4981
zili.wang <zili.wang@phicomm.com>
2018-05-25 11:09:01 +0800

    fix a bug about change the pixel value of img

--- training/train_my_net.py
+++ training/train_my_net.py
@@ -119,8 +119,7 @@ def preprocess_for_train(image,
     image.set_shape([output_height, output_width, 3])
     image = tf.to_float(image)
     image = tf.image.random_flip_left_right(image)
-    #return _mean_image_subtraction(image, [_R_MEAN, _G_MEAN, _B_MEAN])
-    return image
+    return _mean_image_subtraction(image, [_R_MEAN, _G_MEAN, _B_MEAN])
 
 def vgg_16(inputs,
            num_classes=1,

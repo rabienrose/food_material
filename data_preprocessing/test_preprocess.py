1ce8eb5628ff1d9352867795ca217047fa7d47a3
chamo <283136745@qq.com>
2018-05-31 09:29:34 +0800

    add some global var

--- data_preprocessing/test_preprocess.py
+++ data_preprocessing/test_preprocess.py
@@ -1,9 +1,7 @@
 import tensorflow as tf
 import utils.data_helper
+
 _RESIZE_SIDE_MIN = 224
-_R_MEAN = 123.68
-_G_MEAN = 116.78
-_B_MEAN = 103.94
 
 def preprocess_for_train(image,
                          output_height,
@@ -12,7 +10,7 @@ def preprocess_for_train(image,
     image = utils.data_helper._central_crop([image], output_height, output_width)[0]
     image.set_shape([output_height, output_width, 3])
     image = tf.to_float(image)
-    return utils.data_helper._mean_image_subtraction(image, [_R_MEAN, _G_MEAN, _B_MEAN])
+    return utils.data_helper._mean_image_subtraction(image)
 
 class test_preprocess:
     tfrecord_addr=None
@@ -28,5 +26,6 @@ class test_preprocess:
         image, label = utils.data_helper.get_raw_img(self.tfrecord_addr, self.class_num)
         train_image_size = _RESIZE_SIDE_MIN
         image = preprocess_for_train(image, train_image_size, train_image_size)
+        c=50
         images, labels = tf.train.batch([image, label], batch_size=c, num_threads=1,capacity=c*5)
         return images, labels

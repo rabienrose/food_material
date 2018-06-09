75813fb971bd1d6b80e7760d7c6e39646c36b807
peiyuan.luo <peiyuan.luo@phicomm.com>
2018-06-09 20:51:49 +0800

    fix training bug

--- net/mobilenet_v2.py
+++ net/mobilenet_v2.py
@@ -6,7 +6,6 @@ import tensorflow.contrib.slim as slim
 class mobilenet_v2:
     num_classes=None
     is_training = None
-    dropout_keep_prob = None
     spatial_squeeze = None
     scope = None
     fc_conv_padding = None
@@ -15,18 +14,15 @@ class mobilenet_v2:
         print('choose MobileNet_V2')
         self.num_classes = num_classes
         self.is_training = is_training
-        self.dropout_keep_prob = 0.5
         self.spatial_squeeze = True
         self.scope = scope
         self.fc_conv_padding = 'VALID'
         self.global_pool = False
 
     def def_net(self, inputs):
-        with tf.variable_scope(self.scope, 'vgg_16', [inputs], reuse=tf.AUTO_REUSE) as sc:
-            end_points_collection = sc.original_name_scope + '_end_points'
-            with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
-                                outputs_collections=end_points_collection):
-                logits, endpoint = mnv2.mobilenet(inputs, num_classes=self.num_classes, is_training=self.is_training)
-                return logits
+        with tf.contrib.slim.arg_scope(mnv2.training_scope(is_training=self.is_training)):
+            logits, endpoint = mnv2.mobilenet(inputs, num_classes=self.num_classes, reuse=tf.AUTO_REUSE)
+            return logits
+            
 
 

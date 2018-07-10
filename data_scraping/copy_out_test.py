fa8f72410eda4394531a427b4086bf1808c3ca2c
zili.wang <zili.wang@phicomm.com>
2018-07-10 15:01:18 +0800

    copy test

--- data_scraping/copy_out_test.py
+++ data_scraping/copy_out_test.py
@@ -2,14 +2,26 @@ import os
 import numpy as np
 import shutil
 import csv
+folder_root='E:/work/code/transfer/wawacai'
+target_root='E:/work/code/transfer/wawacai_f'
+pre_name='chamo'
 
-folder_root='D:/1156_dish'
-target_root='E:/data/青椒/false'
+def rename_files():
+    folders = os.listdir(folder_root)
+    i = 1000000
+    for item in folders:
+        folder_addr = folder_root + '/' + item
+        dst_folder_addr = folder_root + '/' + pre_name+'_'+str(i)+'.jpg'
+        os.rename(folder_addr, dst_folder_addr)
+        i=i+1
+
+#rename_files()
 number_of_test=1000
 for i in range(number_of_test):
-    folders = os.listdir(folder_root)
-    rand_int = np.random.randint(len(folders))
-    folder_addr=folder_root+'/'+folders[rand_int]
+    #folders = os.listdir(folder_root)
+    #rand_int = np.random.randint(len(folders))
+    #folder_addr=folder_root+'/'+folders[rand_int]
+    folder_addr=folder_root
     imgs = os.listdir(folder_addr)
     rand_int = np.random.randint(len(imgs))
     img_addr=folder_addr+'/'+imgs[rand_int]
